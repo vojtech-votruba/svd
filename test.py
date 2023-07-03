5e37dc66302e492b60e1fe1c74a6ef34c69e182f
vojtavot <55320259+Vojtavot@users.noreply.github.com>
2023-07-03 19:36:34 +0200

    Added the first version

---
+++ test.py
@@ -0,0 +1,34 @@
+from PIL import Image
+import argparse
+import numpy as np
+
+from svd_0.py import svd
+
+def image_compression(path: str, rank: int):
+    image = Image.open(path)
+    img_arr = np.array(image)
+    print(image.size)
+    img_arr_decomposed = [img_arr[:,:,0], img_arr[:,:,1], img_arr[:,:,2]]
+
+    for seq,each in enumerate(img_arr_decomposed):
+        U, Sigma, V_hermit = svd(each)
+        Sigma_Compressed = Sigma #Provisional - to test the algorithm without compression
+
+        result = np.matmul(np.matmul(U,Sigma_Compressed),V_hermit)    
+        img_arr[:,:,seq] = result
+
+    output = Image.fromarray(img_arr)
+    output.save("output.png")
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description="A small implementation of SVD compression.")
+    parser.add_argument('filename', help="You need to parse a filename of the image you want to compress.")
+    parser.add_argument("-r", "--rank", type=float, help="With this parameter you specifiy the rank of the final compressed matrix", default=1)
+    args = parser.parse_args()
+
+    image_compression(args.filename, args.rank)
+
+if __name__ == "__main__":
+    main()
---
+++ versions/svd_0.py
@@ -0,0 +1,36 @@
+'''This implementation is the one we used in our LA class to manualy calculate SVD.
+It is extremly slow, but I wanted to put it here as it is a good demonstration of how the algorithm works'''
+
+import numpy as np
+from sympy import Matrix #We are using Sympy because it doesn't use svd to calculte nullspace https://github.com/sympy/sympy/blob/master/sympy/matrices/subspaces.py
+
+epsilon = 1E-7
+#Simple algorithm to return SVD terms of A = U \Sigma V*
+def svd(A_matrix):
+
+    A_star_A = np.matmul(A_matrix.T, A_matrix) #Matrix of a form A*A
+    eigenvalues, V_matrix = np.linalg.eigh(A_star_A) 
+
+    ind = np.argsort(eigenvalues)
+    eigenvalues = eigenvalues[~ind]
+    V_matrix = V_matrix.T[~ind].T #Sorting the eigenvalues from largest to lowest (with coresponding eigenvectors)
+
+    nonzero_eigen = []
+    for lambda_value in eigenvalues:
+        if lambda_value > epsilon:
+            nonzero_eigen.append(lambda_value)
+    singl_values = np.sqrt(nonzero_eigen) #Calculating singular values
+
+    U_matrix = np.column_stack([np.matmul(A_matrix, V_matrix[:,seq])/value for seq,value in enumerate(singl_values)])  
+    U_sympy = Matrix(U_matrix.T)
+    complement =  np.array(U_sympy.nullspace())
+    for i in range(complement.shape[0]):
+        U_matrix = np.column_stack((U_matrix, complement[i]))
+    Sigma_matrix = np.diagflat(singl_values)
+    while Sigma_matrix.shape[1] < A_matrix.shape[1]:
+        Sigma_matrix = np.column_stack((Sigma_matrix, np.zeros((Sigma_matrix.shape[0],1))))
+
+    while Sigma_matrix.shape[0] < A_matrix.shape[0]:
+        Sigma_matrix = np.row_stack((Sigma_matrix, np.zeros((Sigma_matrix.shape[1],1)).T))
+
+    return [U_matrix, Sigma_matrix,V_matrix.T]
